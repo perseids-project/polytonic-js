725978042e7b394c4a73c0d80e8c9f74c6ac35c2
zfletch <zfletch2@gmail.com>
2019-03-26 14:06:59 -0400

    Updates

---
+++ precache-manifest.8fcd265e6a17d6ce26c5abd6e2b83954.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "35e175fd7692d6484b1e",
+    "url": "/polytonic-js/static/css/main.41f067cf.chunk.css"
+  },
+  {
+    "revision": "35e175fd7692d6484b1e",
+    "url": "/polytonic-js/static/js/main.35e175fd.chunk.js"
+  },
+  {
+    "revision": "82372d024b383ddae0f7",
+    "url": "/polytonic-js/static/js/runtime~main.82372d02.js"
+  },
+  {
+    "revision": "aa1a38d408c17b643340",
+    "url": "/polytonic-js/static/css/2.a0be0c0a.chunk.css"
+  },
+  {
+    "revision": "aa1a38d408c17b643340",
+    "url": "/polytonic-js/static/js/2.aa1a38d4.chunk.js"
+  },
+  {
+    "revision": "aed4bf22386886ec834caa5f172094c2",
+    "url": "/polytonic-js/static/media/tinos-latin-700.aed4bf22.woff2"
+  },
+  {
+    "revision": "7b30eff6751f724235b4b243680584df",
+    "url": "/polytonic-js/static/media/tinos-latin-400italic.7b30eff6.woff2"
+  },
+  {
+    "revision": "fac0d226a0248b5bf601bdf963a06774",
+    "url": "/polytonic-js/static/media/tinos-latin-400italic.fac0d226.woff"
+  },
+  {
+    "revision": "44ed60cdbe6db7de024a4cddf8ea6f50",
+    "url": "/polytonic-js/static/media/tinos-latin-400.44ed60cd.woff2"
+  },
+  {
+    "revision": "add6b49484bb210ee412748289e516ae",
+    "url": "/polytonic-js/static/media/tinos-latin-700italic.add6b494.woff2"
+  },
+  {
+    "revision": "3feeaa37549b4134710f66febebb3c5b",
+    "url": "/polytonic-js/static/media/tinos-latin-700.3feeaa37.woff"
+  },
+  {
+    "revision": "9dd3cad0a4944665a3e4178957a133d0",
+    "url": "/polytonic-js/static/media/tinos-latin-700italic.9dd3cad0.woff"
+  },
+  {
+    "revision": "c4c6014641239a3fd358f5e7bbe5bbf6",
+    "url": "/polytonic-js/static/media/tinos-latin-400.c4c60146.woff"
+  },
+  {
+    "revision": "1383e57eb85ca99948e7ed69efbc1a76",
+    "url": "/polytonic-js/static/media/arimo-latin-700.1383e57e.woff2"
+  },
+  {
+    "revision": "cc7569b29c944be9246be8c439ff9da4",
+    "url": "/polytonic-js/static/media/arimo-latin-400italic.cc7569b2.woff2"
+  },
+  {
+    "revision": "01337cbf8cb58f21e1411c4c9ae23c87",
+    "url": "/polytonic-js/static/media/arimo-latin-400.01337cbf.woff2"
+  },
+  {
+    "revision": "6d15a1bb418e9f34d0606fd2d568b429",
+    "url": "/polytonic-js/static/media/arimo-latin-400.6d15a1bb.woff"
+  },
+  {
+    "revision": "505d2fd4a9acc32cba0c56d7a96e6ed1",
+    "url": "/polytonic-js/static/media/arimo-latin-700italic.505d2fd4.woff"
+  },
+  {
+    "revision": "2e7c6c6f35ee42fbd1264b169410c9bb",
+    "url": "/polytonic-js/static/media/arimo-latin-700italic.2e7c6c6f.woff2"
+  },
+  {
+    "revision": "422dc78ce039a6a6496e2cbb0ed62eb4",
+    "url": "/polytonic-js/static/media/arimo-latin-400italic.422dc78c.woff"
+  },
+  {
+    "revision": "e97a2f2baf901ad2d2c3d7649bffdd22",
+    "url": "/polytonic-js/static/media/arimo-latin-700.e97a2f2b.woff"
+  },
+  {
+    "revision": "dc7d40566bf9f112f7c1b4a296eff567",
+    "url": "/polytonic-js/index.html"
+  }
+];
